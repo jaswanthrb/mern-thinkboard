c73b138fcb84623acbd7bb3653262f38de61a30f
jaswanthrb <jaswanthreddybotta@gmail.com>
2025-07-07 20:17:55 -0700

    prepered for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
@@ -11,12 +12,15 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 
-app.use(cors({
-  // origin: ['http://localhost:5173']
-  origin: '*' // Allow all origins for simplicity; adjust as needed for security
-})); // Enable CORS for all routes
+if(process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    // origin: ['http://localhost:5173']
+    origin: '*' // Allow all origins for simplicity; adjust as needed for security
+  })); // Enable CORS for all routes
+}
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
@@ -34,6 +38,16 @@ app.use((req, res, next) => {
  
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
