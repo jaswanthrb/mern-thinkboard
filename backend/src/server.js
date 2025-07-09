befe9310e98237c8a56896d6089ac87619222563
Jaswanth Reddy Botta <143459319+jaswanthrb@users.noreply.github.com>
2025-07-09 04:14:07 -0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,5 +52,7 @@ if(process.env.NODE_ENV === 'production') {
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log("Render-injected PORT:", process.env.PORT);
+
   });
 });
